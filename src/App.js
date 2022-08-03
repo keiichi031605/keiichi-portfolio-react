ad759816c99d5bf7b0f92720844b5ef1696438cd
keiichi031605 <keichi031605@gmail.com>
2022-08-03 15:58:36 +1000

    added components and built a framework

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+
 export default function App() {
   return (
     <main>
-      {/* <Navbar />
+      <Navbar />
       <About />
       <Projects />
       <Skills />
-      <Testimonials />
-      <Contact /> */}
+      <Experience />
+      <Contact />
     </main>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
