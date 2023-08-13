d82277808a53e52144e878db7b2d493a79192d03
keiichi031605 <keichi031605@gmail.com>
2023-08-13 19:12:59 +1000

    add Header.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import './App.css';
+import './styles/layout.css';
+import Header from './components/Header';
+// import About from './components/About';
+// import Resume from './components/Resume';
+// import Portfolio from './components/Portfolio';
+// import Testimonials from  './components/Testimonials';
+// import ContactUs from './components/ContactUs';
+// import Footer from './components/Footer';
+import resumeData from './resumeData';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header data={this.state.resumeData.main} />
-      <About data={this.state.resumeData.main} />
-      <Resume data={this.state.resumeData.resume} />
-      <Portfolio data={this.state.resumeData.portfolio} />
-      <Contact data={this.state.resumeData.main} />
-      <Footer data={this.state.resumeData.main} /> */}
-    </div>
-  );
+      <Header resumeData={resumeData}/>
+      {/* <About resumeData={resumeData}/>
+      <Resume resumeData={resumeData}/>
+      <Portfolio resumeData={resumeData}/>
+      <Testimonials resumeData={resumeData}/>
+      <ContactUs resumeData={resumeData}/>
+      <Footer resumeData={resumeData}/> */}
+  </div>
+);
 }
 
 export default App;
