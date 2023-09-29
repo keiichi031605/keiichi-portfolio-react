82ce3f925f5304c3fff8a1f7126f0e226d115857
keiichi031605 <keichi031605@gmail.com>
2023-09-29 12:52:05 +1000

    setup app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Home from "./components";
+import Resume from "./components/Resume";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
